c1f621aee30a201a5026366511a9e711bfe45745
magiraldooc <magiraldooc@gmail.com>
2016-12-07 10:10:37 -0500

    Actualización libreria awe.js

--- resources/views/user/previsualize.blade.php
+++ resources/views/user/previsualize.blade.php
@@ -1,6 +1,6 @@
 @extends('user.template.template')
 @section('content')
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html>
 <head>
     <title>AWE Marker AR demo</title>
@@ -19,25 +19,24 @@
             right: 0;
             overflow: hidden;
         }
+        #debugCanvas {
+            position: absolute;
+            z-index: 999;
+            opacity: 0.5;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
     </style>
-    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
 </head>
 <body>
-
-
-
-
-
-<div id="container" style="z-index: -2">
-
-</div>
-
-<script type="text/javascript" src="{{asset('awe.js-master/js/awe-loader-min.js')}}"></script>
+<canvas id="debugCanvas"></canvas>
+<div id="container"></div>
+<script type="text/javascript" src="{{asset('awe/js/awe-v8.js')}}"></script>
+<script type="text/javascript" src="{{asset('awe/js/awe-loader.js')}}"></script>
 <script type="text/javascript">
-    window.addEventListener('load', function()
-    {
-
-
+    //   DEBUG = true;
+    window.addEventListener('load', function() {
         window.awe.init({
             device_type: awe.AUTO_DETECT_DEVICE_TYPE,
             settings: {
@@ -45,150 +44,79 @@
                 default_camera_position: { x:0, y:0, z:0 },
                 default_lights:[
                     {
-                        id: 'point_light',
-                        type: 'point',
-                        color: 0xFFFFFF,
+                        id: 'ambient_light',
+                        type: 'ambient',
+                        color: 0x666666
+                    },
+                    {
+                        id: 'hemi',
+                        type: 'hemisphere',
+                        color: 0xCCCCCC,
                     },
                 ],
             },
             ready: function() {
+                var d = '?_='+Date.now();
+
                 awe.util.require([
                     {
                         capabilities: ['gum','webgl'],
                         files: [
-                            [ '' +
-                            "{{asset('awe.js-master/js/awe-standard-dependencies.js')}}", "{{asset('awe.js-master/js/awe-standard.js')}}"],
-                            "{{asset('awe.js-master/examples/marker_ar/awe-jsartoolkit-dependencies.js')}}",
-                            "{{asset('awe.js-master/examples/marker_ar/awe.marker_ar.js')}}",
+                            // base libraries
+                            [ '{{asset('awe/js/awe-standard-dependencies.js')}}',
+                                '{{asset('awe/js/awe-standard.js')}}'+d],
+                            // plugin dependencies
+                            ['{{asset('awe/awe-jsartoolkit-dependencies.js')}}',
+                                '{{asset('awe/js/plugins/StereoEffect.js')}}',
+                                '{{asset('awe/js/plugins/VREffect.js')}}'],
+                            // plugins
+                            ['{{asset('awe/awe.marker_ar.js')}}'+d,'{{asset('awe/js/plugins/awe.rendering_effects.js')}}'+d]
                         ],
                         success: function() {
-
-
                             awe.setup_scene();
-                            awe.pois.add(
-                                    {
-                                        id:'poi_1',
-                                        position: { x:0, y:0, z:10000 },
-                                        visible: false
-                                    });
-                            awe.projections.add(
-                                    {
-                                        id:'projection_1',
-                                        geometry:
-                                        {
-                                            shape: 'plane',
-                                            height: parseInt("{{$scene["background_height"]}}"),
-                                            width: parseInt("{{$scene["background_width"]}}")
-                                        },
 
-                                        rotation:
-                                        {
-                                            x:270,
-                                            y:0,
-                                            z:0
-                                        },
+                            awe.settings.update({data:{value: 'ar'}, where:{id: 'view_mode'}})
 
-                                        material:
-                                        {
-                                            type: 'phong',
-                                            color: 0xFFFFFF
-                                        },
+                            /*
+                             Binding a POI to a jsartoolkit marker is easy
+                             - First add the awe-jsartoolkit-dependencies.js plugin (see above)
+                             - Then select a marker image you'd like to use
+                             - Then add the matching number as a suffix for your POI id (e.g. _64)
+                             NOTE: See 64.png in this directory or https://github.com/kig/JSARToolKit/blob/master/demos/markers
+                             This automatically binds your POI to that marker id - easy!
+                             */
+                            awe.pois.add({ id:'jsartoolkit_marker_63', position: { x:0, y:0, z:0 }, visible: false });
+                            awe.projections.add({
+                                id:'marker_projection',
+                                geometry: { shape: 'cube', x:50, y:50, z:50 },
+                                position: { x:0, y:0, z:0 },
+                                rotation: { x:0, y:0, z:180 },
+                                material:{ type: 'phong', color: 0xFFFFFF },
+                                texture: { path: '{{asset('awe/awe_orange_square.png')}}' },
+                                visible: false,
+                            }, { poi_id: 'jsartoolkit_marker_63' });
 
-                                        texture:
-                                        {
+                            /*awe.pois.add({ id:'fixed_poi', position: { x:100, y:0, z:-250 }, visible: false });
+                             awe.projections.add({
+                             id:'fixed_projection',
+                             geometry: { shape: 'cube', x:50, y:50, z:50 },
+                             position: { x:0, y:0, z:0 },
+                             rotation: { x:0, y:0, z:0 },
+                             material:{ type: 'phong', color: 0xFFFFFF },
+                             texture: { path: 'awe_orange_square.png' },
+                             }, { poi_id: 'fixed_poi' });*/
 
-                                            path: "{{asset("realidad/tmp/".$scene["background"])}}"
-                                        },
-                                    },
-                                    { poi_id: 'poi_1' });
+                            // animate the fixed POI
+                            /*awe.projections.update({
+                             data:{
+                             animation: { duration: 5, persist: 0, repeat: Infinity },
+                             rotation: { y: 360 },
+                             },
+                             where:{ id:"fixed_projection" },
+                             });*/
 
-                            awe.projections.add(
-                                    {
-                                        id: 'projection_2',
-                                        geometry:
-                                        {
-                                            path: "{{asset("realidad/tmp/".$scene["object"])}}"
-
-                                        },
-
-                                        scale:
-                                        {
-                                            x: parseInt("{{$scene["scale_x"]}}"),
-                                            y: parseInt("{{$scene["scale_y"]}}"),
-                                            z: parseInt("{{$scene["scale_z"]}}")
-                                        },
-
-                                        rotation:
-                                        {
-                                            x:0,
-                                            y:90,
-                                            z:0
-                                        },
-
-                                        position:
-                                        {
-                                            x:parseInt("{{$scene["position_x"]}}"),
-                                            y:parseInt("{{$scene["position_y"]}}"),
-                                            z:parseInt("{{$scene["position_z"]}}")
-                                        },
-
-
-                                        material:
-                                        {
-                                            type: 'phong',
-                                            color: 0xFFFFFF
-                                        },
-                                        texture:
-                                        {
-                                            //path: lista[0].value
-                                            path: "{{asset("realidad/tmp/".$scene["texture"])}}"
-                                        },
-                                    },
-                                    {poi_id: 'poi_1'});
-
-                            awe.events.add([{
-                                id: 'ar_tracking_marker',
-                                device_types: {
-                                    pc: 1,
-                                    android: 1
-                                },
-                                register: function(handler) {
-                                    window.addEventListener('ar_tracking_marker', handler, false);
-                                },
-                                unregister: function(handler) {
-                                    window.removeEventListener('ar_tracking_marker', handler, false);
-                                },
-                                handler: function(event) {
-                                    if (event.detail) {
-                                        if (event.detail['{{$marker}}']) { // we are mapping marker #64 to this projection
-                                            awe.pois.update({
-                                                data: {
-                                                    visible: true,
-                                                    position: { x:0, y:0, z:0 },
-                                                    matrix: event.detail['{{$marker}}'].transform
-                                                },
-                                                where: {
-                                                    id: 'poi_1'
-                                                }
-                                            });
-
-                                            document.getElementById("awe_canvas-0").style.visibility="visible";
-                                        }
-                                        else {
-                                            awe.pois.update({
-                                                data: {
-                                                    visible: false
-                                                },
-                                                where: {
-                                                    id: 'poi_1'
-                                                }
-                                            });
-                                            document.getElementById("awe_canvas-0").style.visibility="hidden";
-                                        }
-                                        awe.scene_needs_rendering = 1;
-                                    }
-                                }
-                            }])
+                            awe.plugins.view('render_effects').enable();
+                            awe.plugins.view('jsartoolkit').enable();
                         },
                     },
                     {
@@ -204,5 +132,6 @@
 </script>
 </body>
 </html>
+
 
 @endsection
